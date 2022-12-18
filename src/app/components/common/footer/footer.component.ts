d20cf0f0b65c366049863819b2488898721eeb51
Eder Joaquin <Ederrr2802@gmail.com>
2022-12-17 23:57:44 -0500

    add button top

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -15,5 +15,10 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
 
   }
+  
+  topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
 }
