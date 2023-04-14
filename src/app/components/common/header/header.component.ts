7e53349eeff7df495f9275d32722c5ba71c15246
Joaquin <ederrr2802@gmail.com>
2023-04-14 15:24:30 -0500

    update template home

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit {
-  public LOGO = "./assets/img/logo.png";
+export class HeaderComponent {
+  public LOGO = "./assets/images/favicons/logo.svg";
   public mybutton: any;
+  public mail: string = "eder.rimarachinr@gmail.com";
+  public MAILTO: string = "mailto:eder.rimarachinr@gmail.com";
 
-  ngOnInit(): void {
-    
-  }
+  isFixedTop = true;
 
-  topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const navbar = this.elementRef.nativeElement.querySelector(".navbar");
+    if (window.pageYOffset > navbar.offsetTop) {
+      this.isFixedTop = false;
+    } else {
+      this.isFixedTop = true;
+    }
   }
 }
